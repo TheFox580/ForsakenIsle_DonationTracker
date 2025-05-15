eb2e7480a9833df6dd1d1ff2a217219eb9abd839
Fox <44817153+TheFox580@users.noreply.github.com>
2025-05-15 20:20:45 +0200

    3.2 : Revives now are based off of targets and not rewards.

--- src/main/java/fr/thefox580/donationbossbar/GoalEvents.java
+++ src/main/java/fr/thefox580/donationbossbar/GoalEvents.java
@@ -34,7 +34,7 @@ public class GoalEvents {
         final JsonObject jsonObject = requestDonorsJson(config.getString("campaign-id"), 10);
         JsonArray data = jsonObject.get("data").getAsJsonArray();
         double dono = amount;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < data.size(); i++) {
             if (dono <= 0) {
                 break;
             } else if (data.get(i).isJsonNull()) {
@@ -45,16 +45,31 @@ public class GoalEvents {
                 Bukkit.getLogger().warning("New £"+donorAmount+" donation");
                 dono -= donorAmount;
                 String donator = donorData.get("donor_name").getAsString();
+                boolean target = !donorData.get("target_id").isJsonNull();
                 String comment = "";
                 if (!donorData.get("donor_comment").isJsonNull()){
                     comment = donorData.get("donor_comment").getAsString();
                 }
-                boolean reward = !donorData.get("reward_claims").isJsonNull();
-                if (reward){
-                    String username = comment;
-                    comment = "";
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(),"revive " + username);
-                    Bukkit.broadcastMessage(colorize(donator + " &ahas revived &2&l" + username));
+                if (target){
+                    String targetId = donorData.get("target_id").getAsString();
+                    final JsonObject jsonObjectTarget = requestTargetsJson(config.getString("campaign-id"), 10);
+                    JsonArray targetData = jsonObjectTarget.get("data").getAsJsonArray();
+                    JsonObject rightTarget = new JsonObject();
+                    for (int j = 0; j < targetData.size(); j++){
+                        if (Objects.equals(targetData.get(j).getAsJsonObject().get("id").getAsString(), targetId)){
+                            rightTarget = targetData.get(j).getAsJsonObject();
+                        }
+                    }
+                    if (!Objects.equals(rightTarget, new JsonObject())){
+                        if (rightTarget.get("title").getAsString().startsWith("Revive a Player")){
+                            String title = rightTarget.get("title").getAsString();
+                            String username = title.substring(title.lastIndexOf(" ")+1);
+                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(),"revive " + username);
+                            Bukkit.broadcastMessage(colorize(donator + " &adonated £"+ donorAmount +" to try and revive &2&l" + username));
+                        }
+                    } else {
+                        Bukkit.getLogger().warning("The target id " + targetId + "could not be found.");
+                    }
                 }
                 Bukkit.getOnlinePlayers().forEach(player -> {
                     player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, SoundCategory.RECORDS, 1, 1);
@@ -105,6 +120,23 @@ public class GoalEvents {
 
     private JsonObject requestDonorsJson(String id, int nbDonos) throws IOException {
         URL url = new URL("https://v5api.tiltify.com/api/public/team_campaigns/" + id + "/donations?limit=" + nbDonos);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        String token = requestToken().getAsJsonObject().get("access_token").getAsString();
+        conn.setRequestProperty(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setRequestMethod("GET");
+
+        JsonElement data;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+            data = JsonParser.parseReader(reader);
+        }
+        if (data == null) throw new RuntimeException("Could not get the response, the data is null.");
+        if (data.isJsonObject()) return data.getAsJsonObject();
+        throw new RuntimeException("Invalid response!");
+    }
+
+    private JsonObject requestTargetsJson(String id, int nbTargets) throws IOException {
+        URL url = new URL("https://v5api.tiltify.com/api/public/team_campaigns/" + id + "/targets?limit=" + nbTargets);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         String token = requestToken().getAsJsonObject().get("access_token").getAsString();
         conn.setRequestProperty(HttpHeaders.AUTHORIZATION, "Bearer " + token);
